dd5c7917d359e5b1a55ae1f61e7e716ab62f201d
Imaad Schuller <imaadschullerr@gmail.com>
2024-07-28 21:48:54 +0200

    edit customers table

--- task_2.sql
+++ task_2.sql
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS Authors (
 
 -- Create the customers table
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_name INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     phone VARCHAR(15),
     address TEXT
 );
