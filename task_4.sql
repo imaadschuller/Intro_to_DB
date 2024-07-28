3fdaa7b15ee18e39cef0f482f078a3990acf0a40
Imaad Schuller <imaadschullerr@gmail.com>
2024-07-28 22:02:05 +0200

    Add description of table books script

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Information'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
