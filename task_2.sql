dfa24ba5afdfe5247932ee85c3afa20f7581d876
Imaad Schuller <imaadschullerr@gmail.com>
2024-07-28 21:53:12 +0200

    edit order_details table

--- task_2.sql
+++ task_2.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     price DECIMAL(10, 2),
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
